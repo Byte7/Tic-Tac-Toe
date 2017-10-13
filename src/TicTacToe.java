8912d06984efca0e4a1c8d93e188892387b1bdbf
Sagar Patil <sagar010797@gmail.com>
2017-10-14 02:33:25 +0530

    added win config,put mark functions
    to add printing board function

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -21,9 +21,26 @@ public class TicTacToe {
 				board[i][j] = EMPTY;
 		player = X;			//First player is 'X'
 	}
-	/*Checking whether given inputs by player is win config or not*/
+	/*Checking whether given inputs by player is win configuration or not*/
 	public boolean isWin(int mark){
-		
+		return ((board[0][0]+board[0][1]+board[0][2] == 3*mark)||	//row 0
+				(board[1][0]+board[1][1]+board[1][2] == 3*mark)||	//row 1
+				(board[2][0]+board[2][1]+board[2][2] == 3*mark)||	//row 2
+				(board[0][0]+board[1][0]+board[2][0] == 3*mark)||	//column 0
+				(board[0][1]+board[1][1]+board[2][1] == 3*mark)||	//column 1
+				(board[0][2]+board[1][2]+board[2][2] == 3*mark)||	//column 2
+				(board[0][0]+board[1][1]+board[2][2] == 3*mark)||	//Main diagonal
+				(board[0][2]+board[1][1]+board[2][0] == 3*mark)		//reverse diagonal
+				);
+	}
+	/*Puts X or O mark on the board position i,j*/
+	public void putMark(int i,int j){
+		if((i<0)||(i>2)||(j<0)||(j>2))
+			throw new IllegalArgumentException("Invalid board position");
+		if(board[i][j] != EMPTY)
+			throw new IllegalArgumentException("Board position not empty,position occupied");
+		board[i][j] = player;	//place the mark on position i,j for current player
+		player = -player;		//Switching player given that O = -X 
 	}
 	/*Returns winning player's code or 0 to indicate a tie game or unfinished game*/
 	public int winner(){
